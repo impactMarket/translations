57c92cafdb7d6f06a92fb52e6caed2c07dd9e94f
Paulo Soares <psoares.bj@gmail.com>
2021-07-27 23:09:15 +0100

    Add FR and PT-BR

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
 const br = require('./translations/br.json');
 const en = require('./translations/en.json');
+const fr = require('./translations/fr.json');
 
 module.exports = {
   br,
-  en
+  en,
+  fr
 };
